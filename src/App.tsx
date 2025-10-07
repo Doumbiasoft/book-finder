c4def2d07d141ef5ec2d95328151e81f50729881
Mouhamed Doumbia <doumbiasoft@gmail.com>
2025-10-06 22:23:45 -0400

    setup my router Provider in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/router";
 
 function App() {
-
   return (
-    <>
- 
-    </>
-  )
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
-export default App
+export default App;
