973f66d9a350dd6b99413c6681f89442ee7e46f5
Mouhamed Doumbia <doumbiasoft@gmail.com>
2025-10-07 01:28:49 -0400

    added book detailed page state management

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,6 +1,16 @@
 import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import type { BookVolume } from "../types/types";
 
 const DetailPage: React.FC = () => {
+  const { bookId } = useParams<{ bookId: string }>();
+  const navigate = useNavigate();
+  const [selectedBook, setSelectedBook] = React.useState<BookVolume | null>(null);
+  const [detailLoading, setDetailLoading] = React.useState<boolean>(false);
+  const [error, setError] = React.useState<string>('');
+
+
+  
   return <div>DetailPage</div>;
 };
 
