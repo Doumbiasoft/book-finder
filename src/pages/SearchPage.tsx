2f2f296086eeba36a801677eb7154ed6188aa18b
Mouhamed Doumbia <doumbiasoft@gmail.com>
2025-10-06 23:04:15 -0400

    added search book states

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,6 +1,17 @@
 import React from "react";
+import type { BookVolume } from "../types/types";
 
 const SearchPage: React.FC = () => {
+   const [query, setQuery] = React.useState<string>('');
+  const [books, setBooks] = React.useState<BookVolume[]>([]);
+  const [loading, setLoading] = React.useState<boolean>(false);
+  const [error, setError] = React.useState<string>('');
+  const [popularBooks, setPopularBooks] = React.useState<BookVolume[]>([]);
+  const [popularLoading, setPopularLoading] = React.useState<boolean>(false);
+  const [searchTimeoutId, setSearchTimeoutId] = React.useState<number | null>(null);
+
+  
+
   return <div>SearchPage</div>;
 };
 
