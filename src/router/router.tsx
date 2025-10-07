7121a0908184ac39d0984bc0843e9938349675a7
Mouhamed Doumbia <doumbiasoft@gmail.com>
2025-10-06 22:44:16 -0400

    updated my routes setup

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,30 +1,20 @@
-import {
-  type RouteObject,
-  createBrowserRouter,
-  Navigate,
-} from "react-router-dom";
+import { type RouteObject, createBrowserRouter } from "react-router-dom";
 import SearchPage from "../pages/SearchPage";
 import DetailPage from "../pages/DetailPage";
 import NotFoundPage from "../pages/NotFoundPage";
 
-const routes: [RouteObject] = [
+const routes: RouteObject[] = [
   {
     path: "/",
-    children: [
-      {
-        path: "/",
-        element: <SearchPage />,
-        children: [
-          {
-            path: "book/:bookId",
-            element: <DetailPage />,
-          },
-          { path: "*", element: <NotFoundPage /> },
-        ],
-      },
-      { path: "/", element: <Navigate to="/" /> },
-      { path: "*", element: <NotFoundPage /> },
-    ],
+    element: <SearchPage />,
+  },
+  {
+    path: "book/:bookId",
+    element: <DetailPage />,
+  },
+  {
+    path: "*",
+    element: <NotFoundPage />,
   },
 ];
 
